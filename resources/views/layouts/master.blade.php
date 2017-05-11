69eb76f0733d4b9bcd4374c12fd4e5f227c95da0
Amir Mokhtar Ali <amirmokhtar@g.harvard.edu>
2017-05-11 23:12:31 +0200

    hunting stray tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,16 +5,12 @@
         <title>
             @yield('title', 'Game')
         </title>
-
         <meta charset='utf-8'>
         <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
         <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
         <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
-
         <link href='/css/gamemaster.css' type='text/css' rel='stylesheet'>
-
         @stack('head')
-
     </head>
     <body>
         <div class="container">
